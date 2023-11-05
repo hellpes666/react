6ceb43a7412c638675ba01d41c3e1766a85f0f71
Hellpes <alexrui_hpow@mail.ru>
2023-11-05 18:08:31 +0300

    react

---
+++ src/components/Content.jsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+    return (
+        <div className='Profile'>
+            <img src='https://i.pinimg.com/originals/f8/4e/7f/f84e7f7cfeeae73511cb3b2d27e39e1f.jpg' />
+          <div>
+            ava + descritption
+          </div>
+
+          <div>my posts</div>
+
+          <div>new post</div>
+
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+    )
+}
+
+
+export default Profile;
---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+const Header = () => {
+    return <header className='header'>
+    <img src="https://images.freeimg.net/rsynced_images/logo-3266214_1280.png" />
+  </header>
+}
+export default Header;
